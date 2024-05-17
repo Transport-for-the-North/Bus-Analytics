ced233d47f7434d982e6c20086b85333ea303a6c
Matt Buckley <matthew.buckley@wsp.com>
2024-05-17 09:28:36 +0100

    OTP fixed bug just running prepare
    Had enabled OTP to only run prepare script instead of running full processing.

--- scheduler.py
+++ scheduler.py
@@ -196,7 +196,7 @@ def produce_cost_metrics(
 
     LOG.info("Starting OTP processing")
     try:
-        otp.run_process(folder=folder, save_parameters=False, prepare=True, force=True)
+        otp.run_process(folder=folder, save_parameters=False, prepare=False, force=True)
     except Exception as exc:
         run_id = pg_database.insert_run_metadata(
             database.ModelName.OTP4GB,
